13684cddef894962ef7d2015e9dc73aaf01f4f58
Massimo Callegari <massimocallegari@yahoo.it>
2018-03-07 21:41:04 +0100

    travis: install missing dependency and attempt to create appimage

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -14,5 +14,12 @@ exit $?
 fi
 if [ "$TASK" = "appimage" ]; then
 $QMAKE CONFIG+=appimage CONFIG+=qmlui && make
+make INSTALL_ROOT=appdir install; find appdir/
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
 exit $?
 fi
