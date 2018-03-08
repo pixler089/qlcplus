7010fbab23e7117727c9b8b9fe3f8dda3f7685ee
Massimo Callegari <massimocallegari@yahoo.it>
2018-03-08 19:51:51 +0100

    travis: attempt to use a different location for installation

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -13,14 +13,17 @@ $QMAKE CONFIG+=coverage && make && ./coverage.sh
 exit $?
 fi
 if [ "$TASK" = "appimage" ]; then
+TARGET_DIR=~/qlcplus.AppDir
 $QMAKE CONFIG+=appimage CONFIG+=qmlui && make
-make INSTALL_ROOT=appdir install; find appdir/
-sed -i -e 's/Exec=qlcplus --open %f/Exec=qlcplus-qml/g' appdir/usr/share/applications/qlcplus.desktop
+make INSTALL_ROOT=$TARGET_DIR install
+find $TARGET_DIR
+cd ~
+sed -i -e 's/Exec=qlcplus --open %f/Exec=qlcplus-qml/g' $TARGET_DIR/usr/share/applications/qlcplus.desktop
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
-./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/qlcplus.desktop -bundle-non-qt-libs
-./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/qlcplus.desktop -appimage
+./linuxdeployqt-continuous-x86_64.AppImage $TARGET_DIR/usr/share/applications/qlcplus.desktop -bundle-non-qt-libs
+./linuxdeployqt-continuous-x86_64.AppImage $TARGET_DIR/usr/share/applications/qlcplus.desktop -appimage
 exit $?
 fi
